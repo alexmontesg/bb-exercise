c0f12b110c098191ab9ffa692a9d4f76595944d8
Alejandro Montes Garcia <amg@caravelo.com>
2021-09-18 18:10:40 +0200

    Init project

---
+++ bot.js
@@ -0,0 +1,56 @@
+//  __   __  ___        ___
+// |__) /  \  |  |__/ |  |  
+// |__) \__/  |  |  \ |  |  
+
+const logger = require('winston');
+const { Botkit } = require('botkit');
+const { FacebookAdapter, FacebookEventTypeMiddleware } = require('botbuilder-adapter-facebook');
+
+// Load process.env values from .env file
+require('dotenv').config();
+
+if (!process.env.FACEBOOK_ACCESS_TOKEN
+  || !process.env.FACEBOOK_VERIFY_TOKEN
+  || !process.env.FACEBOOK_APP_SECRET) {
+  logger.error('Missing fields on .env file. Required fields are: FACEBOOK_ACCESS_TOKEN, FACEBOOK_VERIFY_TOKEN and FACEBOOK_APP_SECRET');
+  process.exit(1);
+}
+
+const adapter = new FacebookAdapter({
+  verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
+  access_token: process.env.FACEBOOK_ACCESS_TOKEN,
+  app_secret: process.env.FACEBOOK_APP_SECRET,
+})
+
+// emit events based on the type of facebook event being received
+adapter.use(new FacebookEventTypeMiddleware());
+
+const controller = new Botkit({
+  webhook_uri: '/api/messages',
+  adapter
+});
+
+// Once the bot has booted up its internal services, you can use them to do stuff.
+controller.ready(() => {
+
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(__dirname + '/features');
+
+  /* catch-all that uses the CMS to trigger dialogs */
+  if (controller.plugins.cms) {
+    controller.on('message,direct_message', async (bot, message) => {
+      let results = false;
+      results = await controller.plugins.cms.testTrigger(bot, message);
+      if (results !== false) {
+        // do not continue middleware!
+        return false;
+      }
+    });
+  }
+});
+
+
+
+controller.webserver.get('/', (req, res) => {
+  res.send(`This app is running Botkit ${controller.version}.`);
+});
